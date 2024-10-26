9eadb19f73b36a785f531badb6b1a57da27e5d3e
RWn-ChetanSharma <rwa5.chetan.sha@gmail.com>
2024-10-26 12:13:04 +0530

    Lecture Data

---
+++ Student/Jeel/Exam.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+main(){
+
+	printf("             A\n");
+	printf("          B  A\n");
+	printf("       C  B  A\n");
+	printf("    D  C  B  A\n");
+	printf("    D  C  B  A\n");
+	printf(" E  D  C  B  A\n");
+				
+	
+	
+	
+};
+ 
+
